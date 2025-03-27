4f1f41f384e21626edda29703369b53db8e0cc50
desmonHak <61366533+desmonHak@users.noreply.github.com>
2025-03-28 00:24:27 +0100

    add dir TEST

---
+++ Test/ServerTest.java
@@ -0,0 +1,22 @@
+package src;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void start() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void stop() {
+    }
+}
